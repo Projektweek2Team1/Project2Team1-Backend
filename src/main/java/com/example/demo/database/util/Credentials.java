d4beef8130e9e39aaaa8f7c1a1007747a3df3e34
matthijs-vr <matthijs.van.riel@gmail.com>
2022-04-20 15:49:38 +0200

    Test environment

--- src/main/java/com/example/demo/database/util/Credentials.java
+++ src/main/java/com/example/demo/database/util/Credentials.java
@@ -6,6 +6,8 @@ public class Credentials {
     static public void setPass(Properties dbProperties) {
         dbProperties.setProperty("user", System.getenv("DB_USER"));
         dbProperties.setProperty("password", System.getenv("DB_USER_PASS"));
+        System.out.println(System.getenv("DB_USER"));
+        System.out.println(System.getenv("DB_USER_PASS"));
 //        dbProperties.setProperty("user", "app_potpal");
 //        dbProperties.setProperty("password", "84P5*q9^4N@NkR^j&jofu@7p");
     }
